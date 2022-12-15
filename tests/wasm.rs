d1292e180e745ce5281bab6f6754ed78349db382
NumberFour8 <lukas.pohanka@inina.net>
2022-12-15 10:14:46 +0100

    WIP on integration tests for timestamps

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -105,5 +105,82 @@ fn test_whole_flow() {
 #[cfg(feature = "timestamps")]
 #[wasm_bindgen_test]
 fn test_whole_flow() {
+    const REQUEST_DATA: &str = "Hello world!";
+    const RESPONSE_DATA: &str = "Hello world from Infura!";
 
+    let mut data_on_wire: Box<[u8]>;
+    let mut client_session: Session;
+    let exit_node_id;
+
+    // --- on RPCh Client ---
+    {
+        // RPCh Client loads the identity of the selected Exit node
+        let exit_node_pk = hex::decode(EXIT_NODE_PUB_KEY).unwrap();
+        exit_node_id = Identity::load_identity(exit_node_pk.as_slice(), None)
+            .expect("client identity load failed");
+
+        let exit_req_counter = 0;
+
+        // RPCh Client create an envelope containing the request and starts up a session
+        client_session = box_request(Envelope::new(REQUEST_DATA.as_bytes(), ENTRY_NODE_PEER_ID, EXIT_NODE_PEER_ID), &exit_node_id, exit_req_counter)
+            .expect("failed to create client session");
+
+        // Now the RPCh Client must update the counter
+        assert_eq!(exit_req_counter + 1,  client_session.updated_counter());
+
+        // session.get_request_data() is sent to the Exit node via HOPR network
+        data_on_wire = client_session.get_request_data().expect("failed to retrieve request data for sending")
+    }
+
+    // --- on Exit node ---
+
+    {
+        // Exit node loads its own identity
+        let exit_pk = hex::decode(EXIT_NODE_PUB_KEY).unwrap();
+        let exit_sk = hex::decode(EXIT_NODE_PRIV_KEY).unwrap();
+        let exit_id = Identity::load_identity(exit_pk.as_slice(), Some(exit_sk.into_boxed_slice()))
+            .expect("exit node identity load failed");
+
+        // Exit node receives the Request data and constructs an Envelope
+        let client_req_counter = 0;
+        let mut session = unbox_request(Envelope::new(data_on_wire.as_ref(), ENTRY_NODE_PEER_ID, EXIT_NODE_PEER_ID), &exit_id, client_req_counter)
+            .expect("request unboxing failed");
+
+        // The Exit node must update the client's counter value in a DB
+        assert_eq!(client_req_counter + 1, session.updated_counter());
+
+        // Now the Exit node performs the request to the Final RPC provider
+        let request_data = session.get_request_data().expect("failed to retrieve request data on exit node");
+        let request_str = String::from_utf8(request_data.into_vec()).expect("failed to decode response string");
+        assert_eq!(REQUEST_DATA, request_str, "message not correct");
+
+        let client_resp_counter = 0;
+
+        // Construct the Response
+        box_response(&mut session, Envelope::new(RESPONSE_DATA.as_bytes(), ENTRY_NODE_PEER_ID, EXIT_NODE_PEER_ID), client_resp_counter)
+            .expect("failed to create response");
+
+        // The Exit node must update the client's counter value in a DB
+        assert_eq!(client_resp_counter + 1, session.updated_counter());
+
+        data_on_wire = session.get_response_data().expect("failed to retrieve response data")
+    }
+
+    // --- on RPCh Client ---
+
+    {
+        let exit_resp_counter = 0;
+
+        unbox_response(&mut client_session, Envelope::new(data_on_wire.as_ref(), ENTRY_NODE_PEER_ID, EXIT_NODE_PEER_ID), exit_resp_counter)
+            .expect("response unboxing failed");
+
+        // Retrieve the response data
+        let response_data = client_session.get_response_data().expect("failed to retrieve response data");
+        let response_str = String::from_utf8(response_data.into_vec()).expect("failed to decode response string");
+
+        assert_eq!(RESPONSE_DATA, response_str, "message not correct");
+
+        // Now the RPCh Client must update the counter
+        assert_eq!(exit_resp_counter + 1,  client_session.updated_counter());
+    }
 }
